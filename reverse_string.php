4082706cf05b65cfa2aac6cb853cf199210ea8d0
arup258 <96279466+arup258@users.noreply.github.com>
2023-03-20 21:18:01 +0530

    Add files via upload

---
+++ reverse_string.php
@@ -0,0 +1,36 @@
+<?php
+// 
+//$n=1;
+//while($n<=20):
+	//echo "$n <br/>";
+	//$n++;
+
+//endwhile;
+?>
+
+
+
+<?php  
+$string = "ARUP";  
+$length = strlen($string);  
+for ($i=($length-1) ; $i >= 0 ; $i--)   
+//{  
+  //echo $string[$i];  
+//}  
+?> 
+
+
+
+
+
+<?php
+$number=13145;
+if($number%2==0)
+{
+  echo"this is a even number";
+}
+else
+{
+  echo"this is not even number";
+}
+
